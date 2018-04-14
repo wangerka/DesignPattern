962b13d94239f04262e4271d19512bb4215a4cf9
gejun <917609824@qq.com>
2018-04-15 01:05:37 +0800

    update

--- Add Two Numbers/src/Main.java
+++ Add Two Numbers/src/Main.java
@@ -19,13 +19,21 @@ public class Main {
 	}
 	
 	private static ListNode buildListNode1(int[] input){
-        ListNode first = null,last = null,newNode;
+        ListNode first = null,last = null,newNode;//三个对象的引用
         if(input.length>0){
             for(int i=0;i<input.length;i++){
+            	/*
+            	 * 1，new ListNode  以ListNode类为模板，在堆空间中创建一个ListNode类对象
+            	 * 2，（）执行构造函数
+            	 * 3，ListNode newNode 创建了一个ListNode类的引用变量，也就是说用来指向ListNode对象的对象引用
+            	 * 4，“=”，这个等号操作符表示使对象引用newNode指向刚创建的ListNode对象。
+            	 */
                 newNode=new ListNode(input[i]);
                 newNode.next=null;
                 if(first==null){
-                	//********************
+                	/*
+                	 * 此时 first last newNode 都指向第一次创建的ListNode类对象
+                	 */
                     first=newNode;
                     last=newNode;
                     //********************
