19a8b02db85572e17810a838bd0cdc19dbb1c0f9
gejun <917609824@qq.com>
2018-04-15 00:43:27 +0800

    update

--- Add Two Numbers/src/Main.java
+++ Add Two Numbers/src/Main.java
@@ -5,6 +5,17 @@ public class Main {
 		int[] a={1,2,3,5,7,8,9};
 		System.out.println(buildListNode1(a).toString());
 //		buildListNode1(a);
+		
+//		
+//		String a1 =null;
+//		String b = null;
+//		String c;
+//		c = new String("hello");
+//		a1 = c;
+//		b = c;
+//		b += "world";
+//		System.out.println(b);
+//		System.out.println(a1);
 	}
 	
 	private static ListNode buildListNode1(int[] input){
@@ -14,8 +25,10 @@ public class Main {
                 newNode=new ListNode(input[i]);
                 newNode.next=null;
                 if(first==null){
+                	//********************
                     first=newNode;
                     last=newNode;
+                    //********************
                 }else{
                     last.next=newNode;
                     last=newNode;
