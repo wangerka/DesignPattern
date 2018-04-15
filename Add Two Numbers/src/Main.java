9120727e8d3d7ff465c44ce56296055101388528
gejun <917609824@qq.com>
2018-04-15 10:23:18 +0800

    update

--- Add Two Numbers/src/Main.java
+++ Add Two Numbers/src/Main.java
@@ -1,9 +1,19 @@
 
 public class Main {
+	/*
+	 * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+		Output: 7 -> 0 -> 8
+		Explanation: 342 + 465 = 807.
+	 */
 	
 	public static void main(String[] args) {
-		int[] a={1,2,3,5,7,8,9};
+		int[] a={2,5,3,4};
+		int[] b ={5,6,4};
 		System.out.println(buildListNode1(a).toString());
+		System.out.println(buildListNode1(b).toString());
+		ListNode l1 = buildListNode1(a);
+		ListNode l2 = buildListNode1(b);
+		System.out.println(addListNode1(l1, l2).toString());
 //		buildListNode1(a);
 		
 //		
@@ -45,5 +55,94 @@ public class Main {
         }
         return first;
     }
+	
+	
+	/*
+	 * if条件判断+递归 会导致 每次都重新执行方法 临时变量会被重置
+	 */
+	static ListNode result = null;
+	static ListNode last = null;
+	static int carry=0;
+	public static ListNode addListNode(ListNode l1, ListNode l2){
+		
+		ListNode curr;
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		int num1=0;
+		int num2=0;
+		int total=0;
+		if(l1 != null || l2 != null || carry != 0){
+			if(l1 != null){
+				num1 = l1.val;
+				l1=l1.next;
+			}
+			if(l2 != null){
+				num2 = l2.val;
+				l2=l2.next;
+			}
+			total = num1+num2+carry;
+			if(total>10){
+				carry =total/10;
+			}
+			curr=new ListNode(total%10);
+			curr.next=null;
+			if(result == null){
+				result = curr;
+				last = curr;
+			} else {
+				last.next = curr;
+				last = curr;
+			}
+			if(l1 !=null || l2!=null){
+				addListNode(l1, l2);
+			}
+		}
+		return result;
+	}
 
+	/*
+	 * 可能的情况有：
+		1.两个链表first和second都没有到表尾；
+		2.一个到表尾，另一个没有；
+		3.两个都到表尾；
+		
+		注意！当一个链表到结尾或者两个链表都到结尾时，但是carry不为0，程序仍不能结束；
+	 */
+	public static ListNode addListNode1(ListNode l1, ListNode l2){
+		ListNode result=null,last=null,newNode;
+		int carry=0;
+
+		int total=0;
+		
+		while(l1!=null || l2 != null || carry!=0){
+			int num1=0;
+			int num2=0;
+			if(l1 !=null){
+				num1 = l1.val;
+				l1=l1.next;
+			}
+			if(l2!=null){
+				num2 = l2.val;
+				l2=l2.next;
+			}
+//			System.out.println(num1+"===="+num2+"======"+carry);
+			total = num1+num2+carry;
+			newNode = new ListNode(total%10);
+			newNode.next=null;
+			if(total>10){
+				carry=1;
+			} else {
+				carry =0;
+			}
+			if(result == null){
+				result = newNode;
+				last = newNode;
+			} else {
+				last.next = newNode;
+				last = newNode;
+			}
+		}
+		
+		return result;
+	}
 }
