be3bbd34136e67967f991e552f21b9aca566b1a1
gejun <917609824@qq.com>
2018-04-15 13:56:50 +0800

    update longest substring

--- LongestSubString/src/Main.java
+++ LongestSubString/src/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -10,8 +12,37 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s ="gfsgsvsvfvfdsdsvd";
-		System.out.println(longestSubString(s));
+		String s ="abcabcbb";
+//		System.out.println(longestSubString(s));
+		System.out.println(longestSubString1(s));
+	}
+	
+	
+	/*
+	 * 
+	 */
+	public static int longestSubString1(String s){
+		int res=0;
+		int leftbound=0;
+		//map保存char和position键值对
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		int length = s.length();
+		for(int i =0;i<length;i++){
+			char c = s.charAt(i);
+			if(map.containsKey(c)){
+				/*
+				 * 当窗口向右滑动，如果新添加的char不包含在map中，则leftbound不变
+				 * 如果包含在map中。则leftbond要么至少+1（新char在原来map的第一位）
+				 * 或者是map中第n个char的位置+1
+				 */
+				leftbound =Math.max(leftbound, map.get(c)+1);
+			}
+			map.put(c,i);
+			System.out.println(c+"========"+leftbound +"===="+map.toString());
+			res = Math.max(res, i-leftbound+1);
+			
+		}
+		return res;
 	}
 	
 	public static int longestSubString(String s){
